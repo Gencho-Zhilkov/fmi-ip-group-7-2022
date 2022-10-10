157e3cb5d02415321fed5e68dd0559c9767f823a
Gencho Zhilkov <gencho.zhilkov@gmail.com>
2022-10-11 00:53:32 +0300

    Add exercises for 10.10.2022

---
+++ 10.10.2022/exercise_1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+
+bool isLeapYear(int year) {
+	return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+}
+
+int daysInMonth(int month, int year) {
+	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+		return 31;
+	} else if (month == 2) {
+		if (isLeapYear(year)) {
+			return 29;
+		} else {
+			return 28;
+		}
+	} else {
+		return 30;
+	}
+}
+
+int main() {
+	int day, month, year;
+	std::cout << "Enter date: ";
+	std::cin >> day >> month >> year;
+	std::cout << day << ' ' << month << ' ' << year << std::endl;
+
+	if (month < 1 || month > 12) {
+		std::cerr << "Wrong month: must be between 1 and 12." << std::endl;
+		return 1;
+	}
+
+	if (day < 1 || day > daysInMonth(month, year)) {
+		std::cerr << "Wrong day: must be between 1 and " << daysInMonth(month, year) << std::endl;
+		return 1;
+	}
+
+	int totalDays = 0;
+	// int currentMonth = 1;
+	// while (currentMonth < month) {
+	// 	totalDays += daysInMonth(currentMonth, year);
+	// 	currentMonth ++;
+	// }
+	for (int currentMonth = 1; currentMonth < month; currentMonth ++) {
+		totalDays += daysInMonth(currentMonth, year);
+	}
+	totalDays += day;
+
+	std::cout << "Day of year is: " << totalDays << std::endl;
+	return 0;
+}
