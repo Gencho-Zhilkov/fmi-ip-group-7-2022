a4c1411c1325ae2c9a98fd6d680bdec114eea5c7
Gencho Zhilkov <gencho.zhilkov@gmail.com>
2022-12-19 17:36:16 +0200

    07.11.2022: fix function name

--- 2022-11-07/exercise_7.cpp
+++ 2022-11-07/exercise_7.cpp
@@ -9,18 +9,18 @@ size_t min(size_t x, size_t y, size_t z) {
 	return min(min(x, y), z);
 }
 
-size_t levenstein_distance(int *xs, size_t xlen, int *ys, size_t ylen) {
+size_t levensteinDistance(int *xs, size_t xlen, int *ys, size_t ylen) {
 	if (xlen == 0) {
 		return ylen;
 	} else if (ylen == 0) {
 		return xlen;
 	} else if (xs[xlen-1] == ys[ylen-1]) {
-		return levenstein_distance(xs, xlen-1, ys, ylen-1);
+		return levensteinDistance(xs, xlen-1, ys, ylen-1);
 	} else {
 		return 1 + min(
-			levenstein_distance(xs, xlen-1, ys, ylen),
-			levenstein_distance(xs, xlen,   ys, ylen-1),
-			levenstein_distance(xs, xlen-1, ys, ylen-1)
+			levensteinDistance(xs, xlen-1, ys, ylen),
+			levensteinDistance(xs, xlen,   ys, ylen-1),
+			levensteinDistance(xs, xlen-1, ys, ylen-1)
 		);
 	}
 }
@@ -29,22 +29,22 @@ int main() {
 	{
 		int xs[] = {1, 2, 3, 4};
 		int ys[] = {1, 2, 3};
-		std::cout << levenstein_distance(xs, 4, ys, 3) << std::endl;
+		std::cout << levensteinDistance(xs, 4, ys, 3) << std::endl;
 	}
 	{
 		int xs[] = {1, 2, 3, 4};
 		int ys[] = {1, 2, 4};
-		std::cout << levenstein_distance(xs, 4, ys, 3) << std::endl;
+		std::cout << levensteinDistance(xs, 4, ys, 3) << std::endl;
 	}
 	{
 		int xs[] = {1, 2, 3, 4};
 		int ys[] = {1, 2, 4, 3};
-		std::cout << levenstein_distance(xs, 4, ys, 4) << std::endl;
+		std::cout << levensteinDistance(xs, 4, ys, 4) << std::endl;
 	}
 	{
 		int xs[] = {1, 2, 3, 4};
 		int ys[] = {1, 2, 4, 5};
-		std::cout << levenstein_distance(xs, 4, ys, 4) << std::endl;
+		std::cout << levensteinDistance(xs, 4, ys, 4) << std::endl;
 	}
 	return 0;
 }
